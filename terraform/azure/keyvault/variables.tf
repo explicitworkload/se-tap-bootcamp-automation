349ebc9bb4f2347beaccbb669362755a66ad5499
Ubuntu <john@ubuntu.gfuycyvztytuhhdo5dhvonhofe.ix.internal.cloudapp.net>
2022-03-10 18:18:23 +0000

    update

--- terraform/azure/keyvault/variables.tf
+++ terraform/azure/keyvault/variables.tf
@@ -1,5 +1,5 @@
 
-variable "resource_group_name" {
+variable "gohjtapworkshop" {
   type        = string
   description = "A name for a resource group; @see https://docs.microsoft.com/en-us/azure/azure-resource-manager/management/manage-resource-groups-portal#what-is-a-resource-group"
 }
